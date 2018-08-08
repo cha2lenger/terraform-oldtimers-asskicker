64b2ef675cff18086c896fea2ee08c781a9ba474
Vlad Orlov <orlov.vladimir@gmail.com>
2018-08-08 07:58:32 -0700

    Next set of changes.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "name" {
    value = "${random_pet.animal.id}"
    description = "Contains the name of a random animal."
 }
+
+output "required-var" {
+   value = "${var.prefix}-${random_pet.animal.id}"
+}
